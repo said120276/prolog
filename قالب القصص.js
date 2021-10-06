ec7fd2264f10dbcfc5e494cf00d44e4dd7a9084c
said120276 <said120276@gmail.com>
2021-10-06 18:51:51 +0100

    Update قالب القصص.js

--- قالب القصص.js
+++ قالب القصص.js
@@ -6380,7 +6380,7 @@ p.nominalBounds = new cjs.Rectangle(-14.9,-8.9,28.8,16.9);
 		
 				that.trk.x = sldWidth * rate;
 				that.rate = rate;
-		        that.parent.mov.gotoAndStop(that.totalFrm * that.rate);
+		        that.parent.mov.gotoAndPlay(that.totalFrm * that.rate);
 			}
 		}
 	}
